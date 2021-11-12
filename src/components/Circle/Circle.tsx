53aba5e7eb82e06d8b32567761d43ff54cba5db0
Tatjana Merkel <tatjana_merkel@outlook.com>
2021-11-12 22:47:19 +0100

    feat: build first d3 circle

--- src/components/Circle/Circle.tsx
+++ src/components/Circle/Circle.tsx
@@ -1,16 +1,30 @@
-import './Circle.css';
+import * as d3 from 'd3';
+import {useEffect, useRef} from "react";
 
 function Circle() {
 
+    const ref: any = useRef()
+
+    useEffect(() => {
+        const svg = d3.select(ref.current).append("svg").attr("width", 200).attr("height", 200)
+
+        svg.append('circle')
+            .attr('cx', 100)
+            .attr('cy', 100)
+            .attr('r', 50)
+            .attr('stroke', 'black')
+            .attr('fill', '#69a3b2');
+
+
+    })
+
 
     return (
-        <svg>
-            <circle
-                cx="150"
-                cy="77"
-                r="25"
-            />
-        </svg>
+        <div ref={ref}>
+
+        </div>
+
+
     );
 }
 
