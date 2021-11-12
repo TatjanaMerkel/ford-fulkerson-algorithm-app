7c4d326346b667707f161708f511cbf088fff99c
Tatjana Merkel <tatjana_merkel@outlook.com>
2021-11-12 22:55:13 +0100

    feat: add svg circle2 component (shorter code)

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import Circle from "./components/Circle/Circle";
+import Circle2 from "./components/Circle/Circle2";
 
 function App() {
     return (
         <div>
             <Circle />
+            <Circle2 />
         </div>
     );
 }
---
+++ src/components/Circle/Circle2.tsx
@@ -0,0 +1,13 @@
+const Circle2 = () => {
+    return (
+        <svg>
+            <circle
+                cx="150"
+                cy="77"
+                r="40"
+            />
+        </svg>
+    )
+}
+
+export default Circle2;
