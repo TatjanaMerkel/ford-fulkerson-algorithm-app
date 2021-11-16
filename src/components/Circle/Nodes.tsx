c4fac5a8df77185874646eaed9c28f1dc78fe4c9
Tatjana Merkel <tatjana_merkel@outlook.com>
2021-11-16 21:55:37 +0100

    feat: add node with a click event

--- src/components/Circle/Nodes.tsx
+++ src/components/Circle/Nodes.tsx
@@ -1,10 +1,12 @@
 import * as d3 from 'd3';
-import {useEffect, useRef, useState} from "react";
+import {useEffect, useRef} from "react";
 import './Nodes.css';
 
 function Nodes(props: any) {
     const colors = d3.schemeCategory10;
     const ref: any = useRef()
+
+    let circle: any;
 
     const nodes = new Array<any>();
 
@@ -18,6 +20,7 @@ function Nodes(props: any) {
             .on('contextmenu', (event) => {
                 event.preventDefault();
             })
+
 
         svg.append('circle')
             .attr('class', 'node')
@@ -35,14 +38,23 @@ function Nodes(props: any) {
     const mousedown = (event: any) => {
         // insert new node with at point
         const point = d3.pointer(event);
-        const node = { x: point[0], y: point[1]};
+        const node = {x: point[0], y: point[1]};
         nodes.push(node);
         console.log(nodes);
+
+        update();
     }
 
+    const update = () => {
+        d3.select("svg").append("circle")
+            .attr("cx", nodes[nodes.length-1].x)
+            .attr("cy", nodes[nodes.length-1].y)
+            .attr("r", 25)
+            .style("fill", "purple");
+    }
 
     return (
-        <div ref={ref} />
+        <div ref={ref}/>
     );
 }
 
