c055ddab8545e68455753aea3a8835d82aa4ee26
Tatjana Merkel <tatjana_merkel@outlook.com>
2021-11-16 17:45:41 +0100

    feat: add mousedown EventHandler in svg field

--- src/components/Circle/Nodes.tsx
+++ src/components/Circle/Nodes.tsx
@@ -3,25 +3,36 @@ import {useEffect, useRef} from "react";
 import './Nodes.css';
 
 function Nodes() {
-    const  colors = d3.schemeCategory10;
+    const colors = d3.schemeCategory10;
     const ref: any = useRef()
+
+
+    function mousedown(event: any) {
+        if (event.type === "mousedown")
+            console.log("Clicked!")
+    }
 
     useEffect(() => {
         const svg = d3.select(ref.current)
             .append("svg")
             .attr("width", 960)
             .attr("height", 500)
-            .style("border","2px solid black")
+            .style("border", "2px solid black")
             // method cancels the event if it is cancelable,
-            .on('contextmenu', (event) => { event.preventDefault(); })
+            .on('contextmenu', (event) => {
+                event.preventDefault();
+            })
 
         svg.append('circle')
             .attr('class', 'node')
             .attr('cx', 100)
             .attr('cy', 100)
             .attr('r', 20)
-            .style('fill', () =>  d3.rgb(colors[2]).brighter().toString() )
+            .style('fill', () => d3.rgb(colors[2]).brighter().toString())
             .style('stroke', () => d3.rgb(colors[2]).darker().toString())
+
+
+        svg.on('mousedown', event => mousedown(event))
     })
 
 
