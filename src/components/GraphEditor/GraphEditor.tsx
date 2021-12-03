9628e28b99411172e73f767ac0f73ec652a15b06
Tatjana Merkel <tatjana_merkel@outlook.com>
2021-12-04 00:05:27 +0100

    feat: Add SVG

--- src/components/GraphEditor/GraphEditor.tsx
+++ src/components/GraphEditor/GraphEditor.tsx
@@ -1,12 +1,30 @@
-import React from 'react'
+import React, {RefObject} from 'react'
+import * as d3 from "d3";
 
 class GraphEditor extends React.Component {
 
+    divRef: RefObject<HTMLDivElement>
+
+    constructor(props: any) {
+        super(props)
+
+        this.divRef = React.createRef()
+    }
+
+    componentDidMount() {
+        const width = 960
+        const height = 500
+
+        const svg = d3.select(this.divRef.current)
+            .append('svg')
+            .attr('width', width)
+            .attr('height', height)
+            .on('contextmenu', (event: Event) => {event.preventDefault()})
+    }
+
     render() {
         return (
-            <div>
-                GraphEditor
-            </div>
+            <div ref={this.divRef}/>
         )
     }
 }
