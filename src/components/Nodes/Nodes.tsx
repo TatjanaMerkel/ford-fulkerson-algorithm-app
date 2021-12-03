1362cab3c88ab24113628fc896693d70c12c885c
Tatjana Merkel <tatjana_merkel@outlook.com>
2021-12-03 22:34:27 +0100

    refactor: Remove semicolons and use single quotes

--- src/components/Nodes/Nodes.tsx
+++ src/components/Nodes/Nodes.tsx
@@ -1,5 +1,5 @@
-import React from "react"
-import * as d3 from "d3"
+import React from 'react'
+import * as d3 from 'd3'
 import './Nodes.css'
 
 interface Node {
@@ -15,33 +15,33 @@ interface Links {
 
 class Nodes extends React.Component<any, any> {
 
-    divRef: React.RefObject<HTMLDivElement>;
-    svg: any;
-    dragLine: any;
-    drag: any;
+    divRef: React.RefObject<HTMLDivElement>
+    svg: any
+    dragLine: any
+    drag: any
 
-    colors = d3.scaleOrdinal(d3.schemeCategory10);
-    nodes = new Array<Node>();
-    links = new Array<Links>();
-    lastNodeId: number = 0;
-    simulation: any;
-    path: any;
-    circles: any;
-    selectedNode: any = null;
+    colors = d3.scaleOrdinal(d3.schemeCategory10)
+    nodes = new Array<Node>()
+    links = new Array<Links>()
+    lastNodeId: number = 0
+    simulation: any
+    path: any
+    circles: any
+    selectedNode: any = null
 
     constructor(props: any) {
-        super(props);
-        this.divRef = React.createRef();
+        super(props)
+        this.divRef = React.createRef()
     }
 
     componentDidMount() {
-        const width = 960;
-        const height = 500;
+        const width = 960
+        const height = 500
 
         this.svg = d3.select(this.divRef.current)
-            .append("svg")
-            .attr("width", width)
-            .attr("height", height)
+            .append('svg')
+            .attr('width', width)
+            .attr('height', height)
             .on('contextmenu', (event) => {
                 event.preventDefault()
             })
@@ -50,14 +50,14 @@ class Nodes extends React.Component<any, any> {
             {id: 0, x: 0, y: 0},
             {id: 1, x: 1, y: 1},
             {id: 2, x: 0, y: 2}
-        ];
+        ]
 
-        this.lastNodeId = 2;
+        this.lastNodeId = 2
 
         this.links = [
             {source: this.nodes[0], target: this.nodes[1]},
             {source: this.nodes[1], target: this.nodes[2]}
-        ];
+        ]
 
         // init D3 force layout
         this.simulation = d3.forceSimulation()
@@ -65,13 +65,13 @@ class Nodes extends React.Component<any, any> {
             .force('charge', d3.forceManyBody().strength(-500))
             .force('x', d3.forceX(width / 2))
             .force('y', d3.forceY(height / 2))
-            .on('tick', () => this.tick());
+            .on('tick', () => this.tick())
 
 
         this.drag = d3.drag()
-            .on("start", this.dragStarted)
-            .on("drag", this.dragged)
-            .on("end", this.dragEnded);
+            .on('start', this.dragStarted)
+            .on('drag', this.dragged)
+            .on('end', this.dragEnded)
 
 
         // define links for graph links
@@ -81,21 +81,21 @@ class Nodes extends React.Component<any, any> {
             .attr('refX', 6)
             .attr('markerWidth', 10)
             .attr('markerHeight', 10)
-            .attr('markerUnits', "userSpaceOnUse")
+            .attr('markerUnits', 'userSpaceOnUse')
             .attr('orient', 'auto')
             .append('svg:path')
             .attr('d', 'M0,-5L10,0L0,5')
-            .attr('fill', '#000');
+            .attr('fill', '#000')
 
 
         // handles to link and node element groups
-        this.path = this.svg.append('svg:g').selectAll('path');
-        this.circles = this.svg.append('svg:g').selectAll('g');
+        this.path = this.svg.append('svg:g').selectAll('path')
+        this.circles = this.svg.append('svg:g').selectAll('g')
 
         // app starts here
         this.svg.on('mousedown', (event: Event) => this.mousedown(event))
 
-        this.restart();
+        this.restart()
     }
 
     dragStarted() {
@@ -112,42 +112,42 @@ class Nodes extends React.Component<any, any> {
 
     tick() {
         this.path.attr('d', (d: any) => {
-            const deltaX = d.target.x - d.source.x;
-            const deltaY = d.target.y - d.source.y;
-            const dist = Math.sqrt(deltaX * deltaX + deltaY * deltaY);
-            const normX = deltaX / dist;
-            const normY = deltaY / dist;
-            const sourcePadding = 12;
-            const targetPadding = 17;
-            const sourceX = d.source.x + (sourcePadding * normX);
-            const sourceY = d.source.y + (sourcePadding * normY);
-            const targetX = d.target.x - (targetPadding * normX);
-            const targetY = d.target.y - (targetPadding * normY);
+            const deltaX = d.target.x - d.source.x
+            const deltaY = d.target.y - d.source.y
+            const dist = Math.sqrt(deltaX * deltaX + deltaY * deltaY)
+            const normX = deltaX / dist
+            const normY = deltaY / dist
+            const sourcePadding = 12
+            const targetPadding = 17
+            const sourceX = d.source.x + (sourcePadding * normX)
+            const sourceY = d.source.y + (sourcePadding * normY)
+            const targetX = d.target.x - (targetPadding * normX)
+            const targetY = d.target.y - (targetPadding * normY)
 
-            return `M${sourceX},${sourceY}L${targetX},${targetY}`;
-        });
+            return `M${sourceX},${sourceY}L${targetX},${targetY}`
+        })
 
-        this.circles.attr('transform', (d: any) => `translate(${d.x},${d.y})`);
+        this.circles.attr('transform', (d: any) => `translate(${d.x},${d.y})`)
     }
 
     restart() {
 
         /* Links */
         // path (link) group
-        this.path = this.path.data(this.links);
+        this.path = this.path.data(this.links)
 
         // add new links
         this.path = this.path.join('svg:path')
             .attr('class', 'link')
-            .merge(this.path);
+            .merge(this.path)
 
 
         /* NODES */
         //binding the nodes
-        this.circles = this.circles.data(this.nodes, (d: any) => d.id);
+        this.circles = this.circles.data(this.nodes, (d: any) => d.id)
 
         // add new nodes
-        const group = this.circles.join('svg:g');
+        const group = this.circles.join('svg:g')
 
         group.append('svg:circle')
             .attr('class', 'node')
@@ -156,7 +156,7 @@ class Nodes extends React.Component<any, any> {
             .style('stroke', (d: any) => d3.rgb(this.colors(d.id)).darker().toString())
             .call(this.drag)
             .on('click', (event: any, d: any) => {
-                    this.selectedNode = d;
+                    this.selectedNode = d
                     this.restart()
                 }
             )
@@ -167,26 +167,26 @@ class Nodes extends React.Component<any, any> {
             .attr('x', 0)
             .attr('y', 4)
             .attr('class', 'id')
-            .text((d: any) => d.id);
+            .text((d: any) => d.id)
 
-        this.circles = group.merge(this.circles);
+        this.circles = group.merge(this.circles)
 
         // set the graph in motion
         // selection.nodes(): return the array of selection that contains HTML elements
         this.simulation
             .nodes(this.nodes)
 
-        this.simulation.alpha(1).restart();
+        this.simulation.alpha(1).restart()
     }
 
     mousedown(event: Event) {
 
         // insert new node at point
-        const point = d3.pointer(event);
-        const node = {id: ++this.lastNodeId, x: point[0], y: point[1]};
-        this.nodes.push(node);
+        const point = d3.pointer(event)
+        const node = {id: ++this.lastNodeId, x: point[0], y: point[1]}
+        this.nodes.push(node)
 
-        this.restart();
+        this.restart()
     }
 
 
@@ -197,4 +197,4 @@ class Nodes extends React.Component<any, any> {
     }
 }
 
-export default Nodes;
+export default Nodes
