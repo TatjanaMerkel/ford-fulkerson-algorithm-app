6908c2197607a99fa795cc59833d7cd1d7fa7763
Tatjana Merkel <tatjana_merkel@outlook.com>
2021-12-03 23:59:12 +0100

    feat: Add GraphEditor component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import Nodes from './components/Nodes/Nodes'
+import GraphEditor from './components/GraphEditor/GraphEditor'
 
 function App() {
     return (
         <div>
             <Nodes/>
+            <hr/>
+            <GraphEditor/>
         </div>
-    );
+    )
 }
 
-export default App;
+export default App
 
---
+++ src/components/GraphEditor/GraphEditor.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class GraphEditor extends React.Component {
+
+    render() {
+        return (
+            <div>
+                GraphEditor
+            </div>
+        )
+    }
+}
+
+export default GraphEditor
