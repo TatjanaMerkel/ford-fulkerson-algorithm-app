c4af4bd01bc6035d5f049bca48111c70df8e4020
Tatjana Merkel <tatjana_merkel@outlook.com>
2021-12-06 01:36:49 +0100

    feat: Remove Nodes component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
-import Nodes from './components/Nodes/Nodes'
 import GraphEditor from './components/GraphEditor/GraphEditor'
 
 function App() {
     return (
         <div>
-            <Nodes/>
-            <hr/>
             <GraphEditor/>
         </div>
     )
 }
 
 export default App
-
