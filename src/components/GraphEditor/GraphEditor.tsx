7f78d09006842d37a860b19f33c2d11e223d0029
Tatjana Merkel <tatjana_merkel@outlook.com>
2021-12-10 14:18:39 +0100

    refactor: Extract event handlers into top level methods

--- src/components/GraphEditor/GraphEditor.tsx
+++ src/components/GraphEditor/GraphEditor.tsx
@@ -17,6 +17,9 @@ interface Link {
 
 class GraphEditor extends React.Component {
 
+    width = 960
+    height = 500
+
     divRef: RefObject<HTMLDivElement>
 
     nodes: Node[] = [
@@ -30,15 +33,11 @@ class GraphEditor extends React.Component {
         {source: this.nodes[1], target: this.nodes[2]}
     ]
 
-    svgLinks!: Selection<any, Link, any, unknown>
-    svgGroups!: Selection<any, Node, any, unknown>
-
-    dragStartNode: null | Node = null
-
     simulation!: Simulation<any, any>
 
-    width = 960
-    height = 500
+    svgLinks!: Selection<any, Link, any, unknown>
+    svgGroups!: Selection<any, Node, any, unknown>
+    dragStartNode: null | Node = null
 
     svgGroupsGroup!: Selection<SVGGElement, unknown, null, undefined>
     svgLinksGroup!: Selection<SVGGElement, unknown, null, undefined>
@@ -57,35 +56,16 @@ class GraphEditor extends React.Component {
             .append('svg')
             .attr('width', this.width)
             .attr('height', this.height)
-            .on('contextmenu', (event: Event) => {
-                event.preventDefault()
-            })
-            .on('mousedown', (event: Event) => this.spawn(event))
-            .on('mousemove', (event: Event) => {
-                if (this.dragStartNode === null) {
-                    return
-                }
-
-                const [x, y] = d3.pointer(event)
-
-                this.svgDragLine
-                    .attr('x1', this.dragStartNode.x)
-                    .attr('y1', this.dragStartNode.y)
-                    .attr('x2', x)
-                    .attr('y2', y)
-            })
-            .on('mouseup', () => {
-                this.svgDragLine.classed('hidden', true)
-                this.dragStartNode = null
-            })
+            .on('contextmenu', (event: Event) => event.preventDefault())
+            .on('mousedown', (event: Event) => this.spawnNode(event))
+            .on('mousemove', (event: Event) => this.moveDragLine(event))
+            .on('mouseup', () => this.cancelDragLine())
 
         this.simulation = d3.forceSimulation(this.nodes)
             .force('charge', d3.forceManyBody().strength(-500))
             .force('x', d3.forceX(this.width / 2))
             .force('y', d3.forceY(this.height / 2))
             .on('tick', () => this.tick())
-
-        this.svgLinksGroup = svg.append('g')
 
         this.svgDragLine = svg.append('line')
             .attr('class', 'dragline hidden')
@@ -94,32 +74,18 @@ class GraphEditor extends React.Component {
             .attr('x2', 0)
             .attr('y2', 0)
 
+        this.svgLinksGroup = svg.append('g')
         this.updateLinks(this.links)
 
         this.svgGroupsGroup = svg.append('g')
-
         this.updateSvgNodes(this.nodes)
 
         this.simulation.restart()
     }
 
-    updateLinks(links: Link[]) {
-        this.updateSvgLinks(links)
-        this.simulation.force('link', d3.forceLink(links).distance(150))
-        this.simulation.alpha(1).restart()
-    }
+    /// Update nodes and links
 
-    updateSvgLinks(links: Link[]): void {
-        this.svgLinks = this.svgLinksGroup.selectAll('.link').data(links)
-
-        const newSvgLink = this.svgLinks
-            .enter().append('line')
-            .classed('link', true)
-
-        this.svgLinks = newSvgLink.merge(this.svgLinks)
-    }
-
-    updateSvgNodes(nodes: Node[]): void {
+    private updateSvgNodes(nodes: Node[]): void {
         this.svgGroups = this.svgGroupsGroup.selectAll('g').data(nodes)
 
         let newSvgGroups = this.svgGroupsGroup.selectAll('g')
@@ -131,29 +97,8 @@ class GraphEditor extends React.Component {
             .attr('r', 20)
             .style('fill', (node: Node) => d3.rgb(this.colors(String(node.color))).brighter().toString())
             .style('stroke', (node: Node) => d3.rgb(this.colors(String(node.color))).darker().toString())
-            .on('mousedown', (event: Event, node: Node) => {
-                this.dragStartNode = node
-
-                this.svgDragLine
-                    .classed('hidden', false)
-                    .attr('x1', node.x)
-                    .attr('y1', node.y)
-                    .attr('x2', node.x)
-                    .attr('y2', node.y)
-            })
-            .on('mouseup', (event: Event, node: Node) => {
-                if (this.dragStartNode === null || this.dragStartNode === node) {
-                    return
-                }
-
-                const sameLinks = this.links.filter((link: Link) =>
-                    link.source === this.dragStartNode && link.target === node)
-
-                if (sameLinks.length === 0) {
-                    this.links.push({source: this.dragStartNode, target: node})
-                    this.updateLinks(this.links)
-                }
-            })
+            .on('mousedown', (event: Event, node: Node) => this.startDragLine(node))
+            .on('mouseup', (event: Event, node: Node) => this.completeDragLine(node))
 
         newSvgGroups.append('text')
             .text((node: Node) => node.name)
@@ -161,7 +106,71 @@ class GraphEditor extends React.Component {
         this.svgGroups = newSvgGroups.merge(this.svgGroups)
     }
 
-    spawn(event: Event): void {
+    private updateLinks(links: Link[]) {
+        this.updateSvgLinks(links)
+        this.simulation.force('link', d3.forceLink(links).distance(150))
+        this.simulation.alpha(1).restart()
+    }
+
+    private updateSvgLinks(links: Link[]): void {
+        this.svgLinks = this.svgLinksGroup.selectAll('.link').data(links)
+
+        const newSvgLink = this.svgLinks
+            .enter().append('line')
+            .classed('link', true)
+
+        this.svgLinks = newSvgLink.merge(this.svgLinks)
+    }
+
+    /// DragLine methods
+
+    private startDragLine(node: Node) {
+        this.dragStartNode = node
+
+        this.svgDragLine
+            .classed('hidden', false)
+            .attr('x1', node.x)
+            .attr('y1', node.y)
+            .attr('x2', node.x)
+            .attr('y2', node.y)
+    }
+
+    private moveDragLine(event: Event) {
+        if (this.dragStartNode === null) {
+            return
+        }
+
+        const [x, y] = d3.pointer(event)
+
+        this.svgDragLine
+            .attr('x1', this.dragStartNode.x)
+            .attr('y1', this.dragStartNode.y)
+            .attr('x2', x)
+            .attr('y2', y)
+    }
+
+    private cancelDragLine() {
+        this.svgDragLine.classed('hidden', true)
+        this.dragStartNode = null
+    }
+
+    private completeDragLine(node: Node) {
+        if (this.dragStartNode === null || this.dragStartNode === node) {
+            return
+        }
+
+        const sameLinks = this.links.filter((link: Link) =>
+            link.source === this.dragStartNode && link.target === node)
+
+        if (sameLinks.length === 0) {
+            this.links.push({source: this.dragStartNode, target: node})
+            this.updateLinks(this.links)
+        }
+    }
+
+    /// spawnNode()
+
+    private spawnNode(event: Event): void {
         if (this.dragStartNode !== null) {
             return
         }
@@ -176,7 +185,9 @@ class GraphEditor extends React.Component {
         this.simulation.alpha(1).restart()
     }
 
-    tick(): void {
+    /// Simulation.tick()
+
+    private tick(): void {
         this.svgLinks
             .attr('x1', (link: Link) => link.source.x)
             .attr('y1', (link: Link) => link.source.y)
@@ -186,6 +197,8 @@ class GraphEditor extends React.Component {
         this.svgGroups
             .attr('transform', (node: Node) => `translate(${node.x},${node.y})`)
     }
+
+    /// render()
 
     render() {
         return (
